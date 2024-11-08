7dc473ac63f0b943f8884136d09cd72203d60979
Alex Defoy <60717559+defoyalex@users.noreply.github.com>
2024-11-07 20:45:48 -0500

    no-cors

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('rsvpForm').addEventListener('submit', function(e) {
     fetch("https://script.google.com/macros/s/AKfycbzuBV9sxzNhKZsEtM2s52I3E_cHNYAO5FwcsxXkl5xfVDBNNaipDpq5D3h0JqfA8PMTNw/exec", {
         method: "POST",
         redirect: "follow",
-        method: "no-cors",
+        mode: "no-cors",
         headers: {
             "Content-Type": "application/json",
         },
